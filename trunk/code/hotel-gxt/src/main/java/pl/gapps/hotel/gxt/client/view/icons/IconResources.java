9646728cc143fca561156cd0b0ee99dd17b937f1
cyprianno <cyprianno@53b28e6a-b016-11de-b4c2-c9e47ae42fda>
2009-11-05 11:38:03 +0000

    'delete' preview; icon resource manager; appspot application name

---
+++ trunk/code/hotel-gxt/src/main/java/pl/gapps/hotel/gxt/client/view/icons/IconResources.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package pl.gapps.hotel.gxt.client.view.icons;
+
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.ImageBundle;
+
+/**
+ * @author cyprian
+ *
+ */
+public interface IconResources extends ImageBundle {
+	@Resource("add16.gif")
+	AbstractImagePrototype add16();
+
+}
---
+++ trunk/code/hotel-gxt/src/main/java/pl/gapps/hotel/gxt/client/view/icons/ResourceManager.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package pl.gapps.hotel.gxt.client.view.icons;
+
+import com.google.gwt.core.client.GWT;
+
+/**
+ * @author cyprian
+ *
+ */
+public class ResourceManager {
+	public static final IconResources ICONS = GWT.create(IconResources.class);
+
+}
